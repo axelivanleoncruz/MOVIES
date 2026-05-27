2456deae3da614e8e43f829903de6ea7b2903b02
León Cruz Axel Ivan <22470169@campeche.tecnm.mx>
2026-05-27 11:27:03 -0600

    cambio el idioma de la interfaz de usuario

--- MOVIES/Models/Movie.cs
+++ MOVIES/Models/Movie.cs
@@ -5,10 +5,14 @@ namespace MOVIES.Models
     public class Movie
     {
         public int id { get; set; }
+        [Display(Name ="Titulo")]
         public string? Title { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name ="Fecha de liberación")]
         public DateTime ReleaseDate { get; set; }
+        [Display(Name ="Genero")]
         public string? Genre { get; set; }
+        [Display(Name ="precio")]
         public decimal Price { get; set; }
     }
 }
